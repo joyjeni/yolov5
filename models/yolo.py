18c4a8437c0b94d880d57f494dfbcae01d82af62
Jenisha <joyjeni@users.noreply.github.com>
2021-02-08 21:03:46 +0530

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -13,7 +13,6 @@ from utils.autoanchor import check_anchor_order
 from utils.general import make_divisible, check_file, set_logging
 from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
-from utils.activations import FReLU
 from torchviz import make_dot
 
 try:
@@ -51,8 +50,8 @@ class Detect(nn.Module):
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
-                #y = x[i].sigmoid()
-                y = x[i].FReLU()
+                y = x[i].sigmoid()
+                #y = x[i].FReLU()
                 y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i].to(x[i].device)) * self.stride[i]  # xy
                 y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                 z.append(y.view(bs, -1, self.no))
